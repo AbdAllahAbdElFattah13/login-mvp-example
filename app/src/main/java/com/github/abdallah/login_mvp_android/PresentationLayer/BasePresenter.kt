a7fa846606bb4bfe34ba547643a9ae55908ae066
AbdAllah Abd-El-Fatah <AbdAllah.A.ELFatah@gmail.com>
2018-04-27 20:34:07 +0200

    Completed LoginContract.

--- app/src/main/java/com/github/abdallah/login_mvp_android/PresentationLayer/BasePresenter.kt
+++ app/src/main/java/com/github/abdallah/login_mvp_android/PresentationLayer/BasePresenter.kt
@@ -1,2 +1,5 @@
 package com.github.abdallah.login_mvp_android.PresentationLayer
 
+interface BasePresenter {
+
+}
--- app/src/main/java/com/github/abdallah/login_mvp_android/PresentationLayer/login/LoginContract.kt
+++ app/src/main/java/com/github/abdallah/login_mvp_android/PresentationLayer/login/LoginContract.kt
@@ -1,4 +1,31 @@
 package com.github.abdallah.login_mvp_android.PresentationLayer.login
 
+import com.github.abdallah.login_mvp_android.DataLayer.Models.LoginModel
+import com.github.abdallah.login_mvp_android.PresentationLayer.BasePresenter
+import com.github.abdallah.login_mvp_android.PresentationLayer.BaseView
+
 interface LoginContract {
+
+    interface View : BaseView {
+        fun setLoading(loading: Boolean)
+
+        fun showEmailErrorMsg()
+
+        fun handleNetworkError()
+
+        fun handleNetworkSuccess()
+
+        fun handleLoginSuccess()
+
+        fun handleLoginFail()
+    }
+
+    interface Presenter : BasePresenter {
+        fun handleOnLoginBtnClick(email: String, password: String)
+
+        fun onSuccess(model: LoginModel)
+
+        fun onError()
+    }
+
 }
