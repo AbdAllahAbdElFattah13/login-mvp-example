bf586dc41bb0b6b7aa798b6d015bdf38aa5b808e
AbdAllah Abd-El-Fatah <AbdAllah.A.ELFatah@gmail.com>
2018-05-20 02:24:15 +0200

    Added Handler.postDelayed to Simulate network call.

--- app/src/main/java/com/github/abdallah/login_mvp_android/DataLayer/DataRepository.kt
+++ app/src/main/java/com/github/abdallah/login_mvp_android/DataLayer/DataRepository.kt
@@ -1,5 +1,6 @@
 package com.github.abdallah.login_mvp_android.DataLayer
 
+import android.os.Handler
 import com.github.abdallah.login_mvp_android.DataLayer.Models.ErrorType
 import com.github.abdallah.login_mvp_android.DataLayer.Models.LoginModel
 import java.util.*
@@ -10,6 +11,8 @@ object DataRepository : Repository {
     //to be used to get a random response to mock the
     //server call success and the server returned result.
     private val mRandom = Random()
+
+    private val SERVICE_LATENCY_IN_MILLIS = 5000L
 
     private fun isEmailValid(email: String): Boolean {
         return Pattern.compile(
@@ -24,26 +27,29 @@ object DataRepository : Repository {
 
     override fun login(userEmail: String, password: String, callbacks: Callbacks.LoginCallbacks) {
 
-        if (!isEmailValid(email = userEmail)) callbacks.onError(ErrorType.InvalidEmail)
+        // Simulate network by delaying the execution.
+        Handler().postDelayed({
+            if (!isEmailValid(email = userEmail)) callbacks.onError(ErrorType.InvalidEmail)
 
-        //so we have 2 booleans,
-        //the first one is being used as a mock up for the
-        //server call. (i.e its true value should be considered
-        //as if the network request returned 200 code.)
-        val isCallToServerSuccessful = mRandom.nextBoolean()
-        if (isCallToServerSuccessful) {
+            //so we have 2 booleans,
+            //the first one is being used as a mock up for the
+            //server call. (i.e its true value should be considered
+            //as if the network request returned 200 code.)
+            val isCallToServerSuccessful = mRandom.nextBoolean()
+            if (isCallToServerSuccessful) {
 
-            //and the second one is being used as a mock up
-            //for the credentials validation. (i.e is user name and
-            //and password is correct?)
+                //and the second one is being used as a mock up
+                //for the credentials validation. (i.e is user name and
+                //and password is correct?)
 
-            //PS: even if the returned result is false, we should
-            //invoke the onSuccess method, because after all
-            //it's a successful network call.
-            val mLoginModel = LoginModel(mRandom.nextBoolean())
-            callbacks.onSuccess(mLoginModel)
-        } else {
-            callbacks.onError(ErrorType.NetworkConnectionError)
-        }
+                //PS: even if the returned result is false, we should
+                //invoke the onSuccess method, because after all
+                //it's a successful network call.
+                val mLoginModel = LoginModel(mRandom.nextBoolean())
+                callbacks.onSuccess(mLoginModel)
+            } else {
+                callbacks.onError(ErrorType.NetworkConnectionError)
+            }
+        }, SERVICE_LATENCY_IN_MILLIS)
     }
 }
